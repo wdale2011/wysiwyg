9f5095df9437ef83fb9e82066f592f92408ee0ff
William Dale <WDale@talentpath.com>
2019-02-19 12:07:38 -0800

    added colors and font-size

--- js/handleEditorInput.js
+++ js/handleEditorInput.js
@@ -4,11 +4,19 @@
  */
 
 export default function handleEditorInput(event) {
+    const textElement = event.currentTarget.parentNode.querySelector('.text-element');
+
     if (event.target.type === 'text' ||
         event.target.tagName === 'TEXTAREA'){
-        
-            const textElement = event.currentTarget.parentNode.querySelector('.text-element');
-            
-            textElement.innerText = event.target.value;
+        //Allows for text change
+            const text = event.target.value;
+            textElement.innerText = text
         }
+    else {
+        // Handle the styles
+        const value = event.target.value
+        const styleName = event.target.name;
+        const unit = event.target.dataset.unit || '';
+        textElement.style[styleName] = `${value}${unit}`
+    }
 }
