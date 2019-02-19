e3235a96235c382381ef6bbf2d7bd6d50ddde7f2
William Dale <WDale@talentpath.com>
2019-02-19 09:54:29 -0800

    Finished edit toggle

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
-// select the edit button
-const editButtons = document.querySelectorAll('.edit-button');
-console.log(editButtons);
-// add event listener (click)
-editButtons.forEach(button => addEventListener('click', toggleActive));
-// function that adds a class to editable-text
-function toggleActive(event) {
-    // get the parent element
-    const editableText = event.target.parentElement;
-    // toggle the class active
-    editableText.classList.toggle('active')
-}
+import toggleActive from './toggleActive.js';
