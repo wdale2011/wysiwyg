a1860eb9e07f5182f6030401af462cdd00ec8566
Andrew P Maney <amaney@usc.edu>
2019-02-19 00:00:20 -0800

    Starter Code

--- js/lib/toggleEditor.js
+++
@@ -1,7 +0,0 @@
-export default function toggleEditor(event) {
-  const editableText = event.target.parentNode;
-  editableText.classList.toggle('active');
-
-  // change button text
-  event.target.innerText = editableText.classList.contains('active') ? 'Save' : 'Edit';
-}
--- js/main.js
+++
@@ -1,8 +0,0 @@
-import toggleEditor from './lib/toggleEditor.js';
-import handleEditorUpdate from './lib/handleEditorUpdate.js';
-import addEditableText from './lib/addEditableText.js';
-
-// attach event listeners
-document.querySelectorAll('.edit-button').forEach(button => button.addEventListener('click', toggleEditor));
-document.querySelectorAll('.editor').forEach(editor => editor.addEventListener('input', handleEditorUpdate));
-document.getElementById('add-button').addEventListener('click', addEditableText);
